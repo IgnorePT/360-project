2420eac5b1a801f1052d9a423db608554eb1617c
Nelson Fernandes <nelson.fernandes@kk.pt>
2022-07-13 19:53:16 +0100

    Landing Page - Create Shared Sections

--- js/app.js
+++ js/app.js
@@ -1,1 +1,21 @@
 console.log("We are online");
+
+//Animations
+gsap.registerPlugin(ScrollTrigger);
+gsap.from('.section-text', {opacity: 0, duration: 1.8, y: -50, ease: "power2.out", delay: 0.4});
+gsap.from('.feature', {scrollTrigger: ".features-list", opacity: 0, duration: 1, y: 50, stagger: 1, ease: "power2.out"})
+// gsap.from('.card_challenge', {scrollTrigger: ".challenge_list", opacity: 0, duration: 1, y: 50, stagger: 0.6, ease: "power2.out"})
+
+
+const highlightsElements = document.getElementsByClassName('highlight');
+
+function replaceHighlightContent(index){
+    
+}
+
+Array.from(highlightsElements).forEach(element => {
+    element.addEventListener('click', function(e){
+        document.querySelector('.highlight.active').classList.remove('active');
+        this.classList.add('active');
+    })
+});
