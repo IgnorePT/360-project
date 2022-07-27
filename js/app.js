47675c0ecdf0a3e2f36347095c4e521bcd447f50
Nelson Fernandes <nelson.fernandes@kk.pt>
2022-07-27 15:15:05 +0100

    Article Page Create

--- js/app.js
+++ js/app.js
@@ -3,30 +3,35 @@ console.log("We are online");
 //Animations
 gsap.registerPlugin(ScrollTrigger);
 
-
-
 /* Feature List */
-gsap.from('.feature', {scrollTrigger: ".features-list", opacity: 0, duration: 1, y: 50, stagger: 1, ease: "power2.out"})
+gsap.from('.feature', { scrollTrigger: ".features-list", opacity: 0, duration: 1, y: 50, stagger: 1, ease: "power2.out" })
 
 
 /* Highlight Content */
 const highlightsElements = document.getElementsByClassName('highlight');
 
-function replaceHighlightContent(index){
-    
+function replaceHighlightContent(index) {
+
 }
 
 Array.from(highlightsElements).forEach(element => {
-    element.addEventListener('click', function(e){
+    element.addEventListener('click', function (e) {
         document.querySelector('.highlight.active').classList.remove('active');
         this.classList.add('active');
     })
 });
 
-document.getElementById('open-hamburger-menu').addEventListener('click',ToogleHamburgerMenu )
-document.getElementById('close-hamburger-menu').addEventListener('click', ToogleHamburgerMenu)
 
-function ToogleHamburgerMenu(){
-    console.log("Open Menu")
+function ToogleHamburgerMenu() {
     document.querySelector('.mobile-menu').classList.toggle('active');
 }
+
+function loadListeners() {
+
+    document.getElementById('open-hamburger-menu').addEventListener('click', ToogleHamburgerMenu)
+    document.getElementById('close-hamburger-menu').addEventListener('click', ToogleHamburgerMenu)
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    loadListeners();
+})
--- js/newsletter.js
+++ js/newsletter.js
@@ -1,32 +1,39 @@
 document.addEventListener("DOMContentLoaded", () => {
 
     const NewsletterEmail = document.getElementById('newsletter-email');
+    const chooseProfileAction = document.querySelectorAll('.choose-profile > custom_checkbox_container > label');
+
+    Array.from(chooseProfileAction).forEach(el => {
+        el.addEventListener('click', function(){
+            console.log('Select Profile')
+            el.classList.toggle('active');
+        })
+    })
+
+
 
     NewsletterEmail.addEventListener('focus', () => {
         document.querySelectorAll('.form-hidden').forEach(el => {
             el.classList.remove('form-none');
-            setTimeout(() => {  
+            setTimeout(() => {
                 el.classList.remove('form-hidden');
             }, 20)
         })
     })
 
-    const NewsletterCiclo = document.getElementById('ciclo-estudo')
+    const NewsletterCiclo = document.getElementById('ciclo-estudo');
 
     const NewsletterActiveSelection = document.getElementsByClassName('active-selection')[0];
-    
+
     NewsletterCiclo.addEventListener('change', (e) => {
-
-
         const node = getCicloElement(e.target.value);
-        NewsletterActiveSelection.innerHTML = NewsletterActiveSelection.innerHTML + node ;
-
+        NewsletterActiveSelection.innerHTML = NewsletterActiveSelection.innerHTML + node;
         NewsletterActiveSelection.value = "";
     })
 
 })
 
-function getCicloElement(ciclo){
+function getCicloElement(ciclo) {
     return `
     <span onclick="removeCiclo(this)">${ciclo} <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
     <path d="M18 6L6 18" stroke="#3B3D3D" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
